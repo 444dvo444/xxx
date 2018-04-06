ca60c541f540fd740e10034df9edaf292777e021
tigran <tigran@localhost.ru>
2018-04-06 17:19:58 +0400

    index.php

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Alexander Hakobyan
+ * Date: 06.04.2018
+ * Time: 17:17
+ * @license http://www.nwslab.com/ NWS Lab product
+ */
+
+echo 'hello world';
